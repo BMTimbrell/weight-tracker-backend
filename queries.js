c44b8236f62ba8d080b7c74896e20a6796d6642e
Ben <bmtimbrell@gmail.com>
2024-02-22 13:34:06 +0000

    changed db connection to connection string

--- queries.js
+++ queries.js
@@ -5,11 +5,7 @@ require("dotenv").config();
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.CONNECITON_STRING
 });
 
 const checkUserAuthorised = (req, res, next) => {
